f72e4680a91518589ae03b010bec0e48157ace42
Rachel Angelyn Gunawan <81099610+Rachel-AG@users.noreply.github.com>
2024-09-12 18:22:08 +0800

    Starter project
    Client: https://github.com/theodorusclarence/ts-nextjs-tailwind-starter
    
    Server:
    https://github.com/hagopj13/node-express-boilerplate

---
+++ client/src/app/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import * as React from 'react';
+import '@/lib/env';
+
+import ArrowLink from '@/components/links/ArrowLink';
+import ButtonLink from '@/components/links/ButtonLink';
+
+import Logo from '~/svg/Logo.svg';
+
+export default function HomePage() {
+  return (
+    <main>
+      <section className='bg-white'>
+        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
+          <Logo className='w-16' />
+          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
+          <p className='mt-2 text-sm text-gray-800'>
+            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
+            Import, Seo, Link component, pre-configured with Husky{' '}
+          </p>
+          <p className='mt-2 text-sm text-gray-700'>
+            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
+              See the repository
+            </ArrowLink>
+          </p>
+
+          <ButtonLink className='mt-6' href='/components' variant='light'>
+            See all components
+          </ButtonLink>
+        </div>
+      </section>
+    </main>
+  );
+}
