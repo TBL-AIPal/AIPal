7059a9565aff48ce9ebc3be31cb6ea20cd2595ee
Rachel Angelyn Gunawan <81099610+Rachel-AG@users.noreply.github.com>
2024-09-13 15:06:47 +0800

    Add API request utility
    - Updated `env.ts` to include `NEXT_PUBLIC_API_URL` with a default empty string fallback.
    - Created a generic `request` function and `get` method in `apiUtils.ts` for API requests.
    - Integrated API request utility into a Next.js page component to fetch and display data.

--- packages/client/src/constant/env.ts
+++ packages/client/src/constant/env.ts
@@ -4,3 +4,5 @@ export const isLocal = process.env.NODE_ENV === 'development';
 export const showLogger = isLocal
   ? true
   : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true' ?? false;
+
+export const apiUrl = process.env.NEXT_PUBLIC_API_URL || '';
