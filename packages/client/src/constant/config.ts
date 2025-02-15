7ed3b3f1421074167367231cf1d8030080a8847f
Rachel Angelyn Gunawan <81099610+Rachel-AG@users.noreply.github.com>
2025-02-15 14:19:19 +0800

    Update logo and site config

--- packages/client/src/constant/config.ts
+++ packages/client/src/constant/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+  title: 'AI Pal',
+  description: 'AI Assistant',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://tsnext-tw.thcl.dev',
 };
