488fa5ee6a40cb40c58cb04943661964cde693d6
Rachel Angelyn Gunawan <81099610+Rachel-AG@users.noreply.github.com>
2025-02-23 00:51:39 +0800

    Update next.config.js
    Fix typo

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: '/api/proxy/:path*',
-        destination: ` http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/v1/:path*`,
+        destination: `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/v1/:path*`,
       },
     ];
   },
